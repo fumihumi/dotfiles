1b653c5bf0552f2f9b09df197251c66489e1e047
fumihumi <t.fum1hum@gmail.com>
2025-01-07 21:55:33 +0900

    update finder.sh

--- setup/20_finder.sh
+++ setup/20_finder.sh
@@ -5,5 +5,7 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
 killall Finder
